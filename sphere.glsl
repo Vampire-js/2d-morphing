879067773dac39b8e91c8c13286bc162e2388f98
Vampire.js <vampirejs.dev@gmail.com>
2025-03-19 15:02:07 +0530

    Added rotation matrix

--- sphere.glsl
+++ sphere.glsl
@@ -16,19 +16,19 @@ struct Cube{
 
 #define MAX_STEPS 64
 #define MAX_DIST 100.
-#define SURF_DIST 0.001
-#define MAX_SPHERES 3
+#define SURF_DIST 0.01
+#define MAX_SPHERES 2
 #define MAX_CUBES 3
-#define butterness 6.
+#define butterness 7.
 
 Sphere spheres[MAX_SPHERES];
 Cube cubes[MAX_CUBES];
 
 void setupSpheres() {
-    spheres[0] = Sphere(vec3(.9*cos(u_time), .8*sin(u_time), 0.05), 0.3);
-    spheres[1] = Sphere(vec3(.9*sin(u_time), .8*cos(u_time), 0.05), 0.3);
+    spheres[0] = Sphere(vec3(.9*cos(u_time), .8*sin(u_time), -0.3), 0.3);
+    spheres[1] = Sphere(vec3(.9*sin(u_time), .8*cos(u_time), -0.), 0.3);
 
-    cubes[0] = Cube(vec3(0.0, 0.0, 0.0), vec3(0.7882, 0.749, 0.1));
+    cubes[0] = Cube(vec3(0.0, 0.0, 0.0), vec3(0.7882, 0.4314, 0.4));
 }
 
 float smin(float a, float b, float k) {
@@ -77,8 +77,21 @@ vec3 getNormal(vec3 p) {
 }
 
 vec3 lighting(vec3 normals, vec3 lightDir, vec3 color) {
-    float ambience = .3;
+    float ambience = .4;
     return mix(color + vec3(ambience), vec3(0.) + vec3(ambience), max(1. - dot(normals, lightDir), 0.));
+}
+
+mat3 rotationMatrix(vec3 axis, float angle) {
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+
+    return mat3(
+        oc * axis.x * axis.x + c,       oc * axis.x * axis.y - axis.z * s,  oc * axis.x * axis.z + axis.y * s,
+        oc * axis.y * axis.x + axis.z * s,  oc * axis.y * axis.y + c,       oc * axis.y * axis.z - axis.x * s,
+        oc * axis.z * axis.x - axis.y * s,  oc * axis.z * axis.y + axis.x * s,  oc * axis.z * axis.z + c
+    );
 }
 
 void main() {
@@ -88,8 +101,8 @@ void main() {
     uv.x *= u_resolution.x / u_resolution.y;
 
     vec3 skyColor = mix(vec3(0.7137, 0.698, 0.698), vec3(1.0, 1.0, 1.0), uv.y);
-    vec3 ro = vec3(0, 0, -2);
-    vec3 rd = normalize(vec3(uv, 0.7));
+    vec3 ro = vec3(-1., 1.3, -2);
+    vec3 rd = rotationMatrix(vec3(0,1.,0), 81.)*normalize(vec3(uv, 0.7));
 
     float t = rayMarch(ro, rd);
     vec3 color = skyColor;
@@ -98,7 +111,7 @@ void main() {
         vec3 p = ro + t * rd;
         vec3 normal = getNormal(p);
         vec3 lightDir = normalize(vec3(0., 1., 00.));
-        float diff = max(lightIntensity * dot(normal, lightDir), 0.0);
+        // float diff = max(lightIntensity * dot(normal, lightDir), 0.0);
         color = lighting(normal, lightDir, vec3(1.0));
     }
 
